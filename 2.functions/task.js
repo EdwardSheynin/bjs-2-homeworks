302f1f0d35451c4271612b0b6416d2a270d6cb79
Edward Sheynin <edwardsheynin@gmail.com>
2022-07-31 11:28:13 +0300

    task.js update

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,53 @@
+'use strict'
+
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
 
+  avg = parseFloat((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
 
-  // Ваш код
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
 
-  // Ваш кода
-  // for ...
+  let max = func(arrOfArr[0]);
+
+  for(const i in arrOfArr) {
+    if (func(arrOfArr[i]) > max) {
+      max = func(arrOfArr[i]);
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  return getArrayParams(arr).max - getArrayParams(arr).min;
 }
