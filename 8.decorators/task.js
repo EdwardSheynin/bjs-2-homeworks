fd6309d1f15dce0876af2efee20a1e802b906ad9
Edward Sheynin <edwardsheynin@gmail.com>
2022-08-15 12:42:23 +0300

    task.js update

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,54 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  
+    function wrapper(...args) {
+        const hash = args.join(',');
+        let objectInCache = cache.find((item) => item['hash'] === hash);
+        if (objectInCache) {
+            console.log('Из кэша: ' + objectInCache['value']);
+            return 'Из кэша: ' + objectInCache['value'];
+        }
+  
+        let result = func(...args);
+        cache.push({'hash': hash, 'value': result});
+        if (cache.length > 5) {
+          cache.shift();
+        }
+        console.log('Вычисляем: ' + result);
+        return 'Вычисляем: ' + result;
+    }
+  
+    return wrapper;
+  }
+
+  const debounceDecoratorNew = (f, ms) => {
+  let timerId;
+  let firstCall = true;
+  return function(...args) {
+    if (firstCall) {
+      timerId = setTimeout(() => {f.apply(this, args)}, 0);
+      firstCall = false;
+    } else {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {f.apply(this, args)}, ms);
+    }
+  }
 }
 
+const debounceDecorator2 = (f, ms) => {
+  let timerId;
+  let firstCall = true;
+  wrapper.count = 0;
+    function wrapper(...args) {
+    if (firstCall) {
+      timerId = setTimeout(() => {f.apply(this, args)}, 0);
+      firstCall = false;
+    } else {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {f.apply(this, args)}, ms);
+    }
+    wrapper.count++;
+  }
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
-
-function debounceDecorator2(func) {
-  // Ваш код
+  return wrapper;
 }
